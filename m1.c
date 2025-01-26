606d78d4a5ebcc518e0a4f3bf76eb34bf9b2961c
RaghulMG <raghulrooney27@gmail.com>
2025-01-26 12:22:12 +0530

    Same file some modified

--- m1.c
+++ m1.c
@@ -5,8 +5,8 @@ int main()
 {
     char str1[20] = "Raghul";
     char str2[20];
-    printf("Befre copy %s\n",str1);
+    printf("Befre cpy: %s\n",str1);
     strcpy(str2,str1);
-    printf("Aftr copy %s\n",str2);
+    printf("Aftr cpy: %s\n",str2);
     return 0;
 }
